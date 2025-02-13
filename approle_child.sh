a2a889afac6ff50a831942f1e9870b4748ea86fe
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-02-13 15:11:49 -0500

    completing comment

--- approle_child.sh
+++ approle_child.sh
@@ -1,6 +1,6 @@
 token1=$(curl -s -k -H -v --request POST --data '{ "role_id": "REDACTED", "secret_id": "REDACTED" }' ${VAULT_ADDR}/v1/auth/approle/login | jq -r .auth.client_token)
 echo $token1
 
-# approle above must have 
+# approle above must have permissions for create-orphan path
 token2=$(curl -s -X POST -H "X-Vault-Request: true" -H "X-Vault-Token: $token1" -d '{"policies":["kv-all"], "ttl":"1h"}' $VAULT_ADDR/v1/auth/token/create-orphan | jq -r .auth.client_token)
 echo $token2
